994704322fc4d028fa5a62bedb78210cc529d6fd
ThisKwasior <3619041+ThisKwasior@users.noreply.github.com>
2025-05-08 22:41:52 +0200

    [core] More getters and setters in sexml

--- kwaslib/core/data/text/sexml_io.c
+++ kwaslib/core/data/text/sexml_io.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void sexml_text_to_entity_references(SU_STRING* text)
 {
@@ -148,6 +149,10 @@ const uint8_t sexml_does_child_exists(SEXML_ELEMENT* element, const char* name)
     return exists;
 }
 
+/*
+    Getters by id
+*/
+
 SEXML_ELEMENT* sexml_get_element_by_id(SEXML_ELEMENT* element, const uint64_t id)
 {
     return *(SEXML_ELEMENT**)cvec_at(element->elements, id);
@@ -157,6 +162,34 @@ SEXML_ATTRIBUTE* sexml_get_attribute_by_id(SEXML_ELEMENT* element, const uint64_
 {
     return *(SEXML_ATTRIBUTE**)cvec_at(element->attributes, id);
 }
+
+const uint8_t sexml_get_attribute_bool_by_id(SEXML_ELEMENT* element, const uint64_t id)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_id(element, id);
+    return sexml_get_attribute_bool(attrib);
+}
+
+const uint64_t sexml_get_attribute_uint_by_id(SEXML_ELEMENT* element, const uint64_t id)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_id(element, id);
+    return sexml_get_attribute_uint(attrib);
+}
+
+const int64_t sexml_get_attribute_int_by_id(SEXML_ELEMENT* element, const uint64_t id)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_id(element, id);
+    return sexml_get_attribute_int(attrib);
+}
+
+const double sexml_get_attribute_double_by_id(SEXML_ELEMENT* element, const uint64_t id)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_id(element, id);
+    return sexml_get_attribute_double(attrib);
+}
+
+/*
+    Getters by name
+*/
 
 SEXML_ELEMENT* sexml_get_element_by_name(SEXML_ELEMENT* element, const char* name)
 {
@@ -204,6 +237,35 @@ SEXML_ATTRIBUTE* sexml_get_attribute_by_name(SEXML_ELEMENT* element, const char*
     return attrib;
 }
 
+const uint8_t sexml_get_attribute_bool_by_name(SEXML_ELEMENT* element, const char* name)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_name(element, name);
+    return sexml_get_attribute_bool(attrib);
+}
+
+const uint64_t sexml_get_attribute_uint_by_name(SEXML_ELEMENT* element, const char* name)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_name(element, name);
+    return sexml_get_attribute_uint(attrib);
+}
+
+const int64_t sexml_get_attribute_int_by_name(SEXML_ELEMENT* element, const char* name)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_name(element, name);
+    return sexml_get_attribute_int(attrib);
+}
+
+const double sexml_get_attribute_double_by_name(SEXML_ELEMENT* element, const char* name)
+{
+    SEXML_ATTRIBUTE* attrib = sexml_get_attribute_by_name(element, name);
+    return sexml_get_attribute_double(attrib);
+}
+
+
+/*
+    Removers
+*/
+
 void sexml_remove_element_by_id(SEXML_ELEMENT* element, const uint64_t id)
 {
     SEXML_ELEMENT* elem = sexml_get_element_by_id(element, id);
@@ -227,6 +289,10 @@ void sexml_remove_attribute_by_id(SEXML_ELEMENT* element, const uint64_t id)
         cvec_erase(element->attributes, id);
     }
 }
+
+/*
+    Setters
+*/
 
 void sexml_set_element_name(SEXML_ELEMENT* element, const char* name)
 {
@@ -277,23 +343,6 @@ void sexml_set_element_text_double(SEXML_ELEMENT* element, const double value, c
     sexml_set_element_text(element, value_str);
 }
 
-SEXML_ELEMENT* sexml_append_element(SEXML_ELEMENT* element, const char* name)
-{
-    SEXML_ELEMENT* elem = sexml_alloc_element();
-    sexml_alloc_element_fields(elem);
-    
-    elem->parent = element;
-    
-    if(name != NULL)
-    {
-        su_insert_char(elem->name, 0, name, strlen(name));
-    }
-    
-    cvec_push_back(element->elements, &elem);
-    
-    return elem;
-}
-
 void sexml_set_attribute_name(SEXML_ATTRIBUTE* attribute, const char* name)
 {
     if(name)
@@ -341,6 +390,23 @@ void sexml_set_attribute_value_double(SEXML_ATTRIBUTE* attribute, const double v
     char value_str[64] = {0};
     snprintf(value_str, 64, "%.*lf", precision, value);
     sexml_set_attribute_value(attribute, value_str);
+}
+
+SEXML_ELEMENT* sexml_append_element(SEXML_ELEMENT* element, const char* name)
+{
+    SEXML_ELEMENT* elem = sexml_alloc_element();
+    sexml_alloc_element_fields(elem);
+    
+    elem->parent = element;
+    
+    if(name != NULL)
+    {
+        su_insert_char(elem->name, 0, name, strlen(name));
+    }
+    
+    cvec_push_back(element->elements, &elem);
+    
+    return elem;
 }
 
 SEXML_ATTRIBUTE* sexml_append_attribute(SEXML_ELEMENT* element, const char* name, const char* value)
@@ -394,9 +460,29 @@ SEXML_ATTRIBUTE* sexml_append_attribute_int(SEXML_ELEMENT* element, const char* 
 SEXML_ATTRIBUTE* sexml_append_attribute_double(SEXML_ELEMENT* element, const char* name, const double value, const uint8_t precision)
 {
     char value_str[64] = {0};
+    
+    /* Just so we don't write 0.00000000000000000000000000000000000 */
+    const double truncated = trunc(value);
+    double temp = 0.f;
     snprintf(value_str, 64, "%.*lf", precision, value);
+    sscanf(value_str, "%lf", &temp);
+    
+    /* Precision check */
+    if(truncated == temp) 
+    {
+        snprintf(value_str, 64, "%.*lf", 0, value);
+    }
+    else
+    {
+        snprintf(value_str, 64, "%.*lf", precision, value); 
+    }
+    
     return sexml_append_attribute(element, name, value_str);
 }
+
+/*
+    Attribute converters
+*/
 
 const uint8_t sexml_get_attribute_bool(SEXML_ATTRIBUTE* attribute)
 {
--- kwaslib/core/data/text/sexml_io.h
+++ kwaslib/core/data/text/sexml_io.h
@@ -12,21 +12,48 @@ const uint64_t sexml_get_element_depth(SEXML_ELEMENT* element);
 const uint64_t sexml_get_child_count(SEXML_ELEMENT* element, const char* name);
 const uint8_t sexml_does_child_exists(SEXML_ELEMENT* element, const char* name);
 
+/*
+    Getters by id
+*/
 SEXML_ELEMENT* sexml_get_element_by_id(SEXML_ELEMENT* element, const uint64_t id);
 SEXML_ATTRIBUTE* sexml_get_attribute_by_id(SEXML_ELEMENT* element, const uint64_t id);
+const uint8_t sexml_get_attribute_bool_by_id(SEXML_ELEMENT* element, const uint64_t id);
+const uint64_t sexml_get_attribute_uint_by_id(SEXML_ELEMENT* element, const uint64_t id);
+const int64_t sexml_get_attribute_int_by_id(SEXML_ELEMENT* element, const uint64_t id);
+const double sexml_get_attribute_double_by_id(SEXML_ELEMENT* element, const uint64_t id);
 
+/*
+    Getters by name
+*/
 SEXML_ELEMENT* sexml_get_element_by_name(SEXML_ELEMENT* element, const char* name);
 SEXML_ATTRIBUTE* sexml_get_attribute_by_name(SEXML_ELEMENT* element, const char* name);
+const uint8_t sexml_get_attribute_bool_by_name(SEXML_ELEMENT* element, const char* name);
+const uint64_t sexml_get_attribute_uint_by_name(SEXML_ELEMENT* element, const char* name);
+const int64_t sexml_get_attribute_int_by_name(SEXML_ELEMENT* element, const char* name);
+const double sexml_get_attribute_double_by_name(SEXML_ELEMENT* element, const char* name);
 
+/*
+    Removers
+*/
 void sexml_remove_element_by_id(SEXML_ELEMENT* element, const uint64_t id);
 void sexml_remove_attribute_by_id(SEXML_ELEMENT* element, const uint64_t id);
 
+/*
+    Setters
+*/
 void sexml_set_element_name(SEXML_ELEMENT* element, const char* name);
 void sexml_set_element_text(SEXML_ELEMENT* element, const char* text);
 void sexml_set_element_text_bool(SEXML_ELEMENT* element, const uint8_t value);
 void sexml_set_element_text_uint(SEXML_ELEMENT* element, const uint64_t value);
 void sexml_set_element_text_int(SEXML_ELEMENT* element, const int64_t value);
 void sexml_set_element_text_double(SEXML_ELEMENT* element, const double value, const uint8_t precision);
+
+void sexml_set_attribute_name(SEXML_ATTRIBUTE* attribute, const char* name);
+void sexml_set_attribute_value(SEXML_ATTRIBUTE* attribute, const char* value);
+void sexml_set_attribute_value_bool(SEXML_ATTRIBUTE* attribute, const uint8_t value);
+void sexml_set_attribute_value_uint(SEXML_ATTRIBUTE* attribute, const uint64_t value);
+void sexml_set_attribute_value_int(SEXML_ATTRIBUTE* attribute, const int64_t value);
+void sexml_set_attribute_value_double(SEXML_ATTRIBUTE* attribute, const double value, const uint8_t precision);
 
 /*
     Appends element and allocates all fields to default values.
@@ -35,13 +62,6 @@ void sexml_set_element_text_double(SEXML_ELEMENT* element, const double value, c
     Returns a pointer to the allocated element.
 */
 SEXML_ELEMENT* sexml_append_element(SEXML_ELEMENT* element, const char* name);
-
-void sexml_set_attribute_name(SEXML_ATTRIBUTE* attribute, const char* name);
-void sexml_set_attribute_value(SEXML_ATTRIBUTE* attribute, const char* value);
-void sexml_set_attribute_value_bool(SEXML_ATTRIBUTE* attribute, const uint8_t value);
-void sexml_set_attribute_value_uint(SEXML_ATTRIBUTE* attribute, const uint64_t value);
-void sexml_set_attribute_value_int(SEXML_ATTRIBUTE* attribute, const int64_t value);
-void sexml_set_attribute_value_double(SEXML_ATTRIBUTE* attribute, const double value, const uint8_t precision);
 
 /*
     Appends an attribute.
@@ -56,6 +76,10 @@ SEXML_ATTRIBUTE* sexml_append_attribute_bool(SEXML_ELEMENT* element, const char*
 SEXML_ATTRIBUTE* sexml_append_attribute_uint(SEXML_ELEMENT* element, const char* name, const uint64_t value);
 SEXML_ATTRIBUTE* sexml_append_attribute_int(SEXML_ELEMENT* element, const char* name, const int64_t value);
 SEXML_ATTRIBUTE* sexml_append_attribute_double(SEXML_ELEMENT* element, const char* name, const double value, const uint8_t precision);
+
+/*
+    Attribute converters
+*/
 
 /* Checks for 1TtYy as first character */
 const uint8_t sexml_get_attribute_bool(SEXML_ATTRIBUTE* attribute);
